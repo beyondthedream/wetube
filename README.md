426b567a309095a1d9739ed913a8401340638fde
Yena Kwon <ynkwon@softrain.co.kr>
2022-08-15 04:26:23 +0900

    router, controller, pug added

---
+++ README.md
@@ -0,0 +1,16 @@
+<!-- @format -->
+
+/ -> Home
+/join -> Join
+/login -> login
+/search -> search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 /** @format */
 import express from "express";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 const PORT = 4000;
 const app = express();
+// const logger = morgan("dev");
+// app.use(logger);
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => {
   console.log(`✅ Server is listening on port http://localhost:${PORT} 🚀`);
